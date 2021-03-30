07a96579e673b6163207b978bc8c040152b8c4bb
Yash9820 <46272410+Yash9820@users.noreply.github.com>
2021-03-30 16:25:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from main import app
+
 class TestHello(unittest.TestCase):
     def setUp(self):
         app.testing = True
@@ -11,14 +12,9 @@ class TestHello(unittest.TestCase):
     def test_login(self):
         rv = self.app.get('/login')
         self.assertEqual(rv.status, '200 OK')
-    
-    
-    
     def test_upload(self):
         rv = self.app.get('/analysis')
         self.assertEqual(rv.status, '200 OK')
-    def test_logout(self):
-        rv = self.app.get('/logout')
-        self.assertEqual(rv.status, '200 OK')
+    
 if __name__ == '__main__':
     unittest.main()
